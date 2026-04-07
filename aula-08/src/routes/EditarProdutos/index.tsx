4b02ccf6364b8aba9295ddcbf9cee2db93fb3735
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-04-07 11:36:28 -0300

    feat : Tipagem de parâmetros.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- aula-08/src/routes/EditarProdutos/index.tsx
+++ aula-08/src/routes/EditarProdutos/index.tsx
@@ -1,9 +1,16 @@
 import { useParams } from "react-router-dom"
 
+//Utilizando type para garantir que o id seja do tipo string:
+type TipoString = {
+  id: string;
+}
+
+
 export default function EditarProdutos() {
 
   //Realizando um destructuring para pegar o id da url:
-  const { id } = useParams();
+  //Utilizando o tipo TipoString para garantir que o id seja do tipo string:
+  const { id } = useParams<TipoString>();
   console.log(`ID do produto a ser editado: ${id}`);
 
 
