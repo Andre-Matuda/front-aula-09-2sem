49600fdf47cc83a7675c3c4d6c4d86376f7cf194
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-04-07 11:21:24 -0300

    feat : Criando o componente OUtlet e organizando as rotas no APP.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- aula-08/src/App.tsx
+++ aula-08/src/App.tsx
@@ -1,8 +1,21 @@
+import { Outlet } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      <h1>Olá, mundo!</h1>
+       <header>
+        <h1>Minha Aplicação</h1>
+        <nav>
+          <a href="/">Home</a> | 
+          <a href="/produtos"> Produtos</a>
+        </nav>
+       </header>
+       <div className="container">
+         <Outlet/> {/* Renderiza o componente da rota filha aqui */}
+       </div>
+        <footer>
+          <p>&copy; 2024 Minha Aplicação</p>
+        </footer>
     </div>
-  )
+  );
 }
